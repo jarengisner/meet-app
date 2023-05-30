a3a02004245f7101df8b74e25d8f458bac64b741
jarengisner <124743127+jarengisner@users.noreply.github.com>
2023-05-30 16:04:00 -0700

    Added tests for search component and suggestions

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,12 +4,21 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
+import CitySearch from '../CitySearch';
 
 describe('<App /> component', () => {
+  //creates AppWrapper variable, allowing us to only write it once//
+  let AppWrapper;
+  beforeAll(() => {
+    AppWrapper = shallow(<App />);
+  });
   test('render list of events', () => {
-    //imports our app as a shallow merge//
-    const AppWrapper = shallow(<App />);
     //tests to see that our app is displaying events//
     expect(AppWrapper.find(EventList)).toHaveLength(1);
+  });
+
+  test('render CitySearch', () => {
+    //tests to see if our search component is rendered in our app//
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
   });
 });

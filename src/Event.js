4ffdc2dca1a64bba340fc141e3654e5e02fcd8d2
jarengisner <124743127+jarengisner@users.noreply.github.com>
2023-05-31 18:41:52 -0700

    Added tests for suggestions function, began adding tests for Event cards

--- src/Event.js
+++ src/Event.js
@@ -1,10 +1,17 @@
 // src/Event.js
 
 import React, { Component } from 'react';
+import { mockData } from './mock-data';
 
 class Event extends Component {
   render() {
-    return <div></div>;
+    //Need to add something along the lines of events.forEach produce a div etc.//
+    //Check if we can use the filtered data, to make sure that once data is filtered we only see those items//
+    return (
+      <div>
+        <h1></h1>
+      </div>
+    );
   }
 }
 export default Event;
---
+++ src/Event.test.js
@@ -0,0 +1,5 @@
+describe('<Event />', () => {
+  test('Event components exist for each event', () => {
+    //Add test here that checks that for each event a seperate component is rendered//
+  });
+});

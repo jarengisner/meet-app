65df6fde2822f56d6f982e40d51bc81c87f68465
jarengisner <124743127+jarengisner@users.noreply.github.com>
2023-06-04 15:47:47 -0700

    Added test to check whether state is initalized at correct number

--- src/__tests__/NumOfEvents.test.js
+++ src/__tests__/NumOfEvents.test.js
@@ -5,12 +5,15 @@ import { shallow } from 'enzyme';
 describe('<NumberOfEvents /> component', () => {
   let numberOfEventsWrapper, numberSelector;
   beforeAll(() => {
-    numberOfEventsWrapper = shallow(<NumberOfEvents eventNum={32} />);
+    numberOfEventsWrapper = shallow(<NumberOfEvents eventNum={() => {}} />);
     numberSelector = numberOfEventsWrapper.find('.numOfEvents');
   });
   test('<NumberOfEvents /> and noe-input are both rendered', () => {
     expect(numberOfEventsWrapper).toBeDefined();
     expect(numberSelector).toBeDefined();
+  });
+  test('num state is 32 by default', () => {
+    expect(numberOfEventsWrapper.state()).toBe(32);
   });
   test('Changing number of events to render', () => {
     const eventObject = { target: { value: 20 } };

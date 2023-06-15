2bc442dad9d9215e45fd6bde5d27f81cecec98f7
jarengisner <124743127+jarengisner@users.noreply.github.com>
2023-06-15 12:20:35 -0700

    Bug fixes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
 import { render } from 'react-dom';
 import * as atatus from 'atatus-spa';
 atatus.config('95e8786a34c9471a95b3fd3e891593e5').install();
@@ -11,5 +10,3 @@ const root = document.getElementById('root');
 render(<App />, root);
 
 serviceWorkerRegistration.register();
-
-reportWebVitals();

82b9259886b70c73ff7d51c9585539c0109f18a6
jarengisner <124743127+jarengisner@users.noreply.github.com>
2023-06-15 12:07:37 -0700

    fixing service worker

--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ atatus.config('95e8786a34c9471a95b3fd3e891593e5').install();
 const root = document.getElementById('root');
 render(<App />, root);
 
-serviceWorker.register();
+serviceWorkerRegistration.register();
 
 reportWebVitals();
